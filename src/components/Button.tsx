9d3edf0fe6b9c7c7624a2d46298c959fb01a6bdb
Pluto0104 <fed.dev.pluto@gmail.com>
2023-09-23 15:56:41 -0500

    create Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import clsx from "clsx";
+import Link from "next/link";
+import React, { AnchorHTMLAttributes } from "react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  color?: "default" | "info" | "success" | "warning" | "error";
+  size?: "xs" | "sm" | "md" | "lg" | "xl";
+  className?: string;
+  href?: string;
+  children: React.ReactNode;
+  onClick?: VoidFunction;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  color = "default",
+  size = "md",
+  className,
+  href,
+  children,
+  onClick = () => {},
+  ...rest
+}) => {
+  const btnClassNames = clsx(
+    "transition duration-150 ease-in-out rounded",
+    {
+      "bg-blue-500 hover:bg-blue-700 text-white": color === "info",
+      "bg-green-500 hover:bg-green-700 text-white": color === "success",
+      "bg-yellow-500 hover:bg-yellow-700 text-white": color === "warning",
+      "bg-red-500 hover:bg-red-700 text-white": color === "error",
+      "py-1 px-2 text-xs": size === "xs",
+      "py-1 px-3 text-sm": size === "sm",
+      "py-2 px-4 text-md": size === "md",
+      "py-3 px-5 text-lg": size === "lg",
+      "py-4 px-6 text-xl": size === "xl",
+    },
+    className
+  );
+
+  return href ? (
+    <Link
+      href={href}
+      className={btnClassNames}
+      {...(rest as AnchorHTMLAttributes<HTMLAnchorElement>)}
+    >
+      {children}
+    </Link>
+  ) : (
+    <button className={btnClassNames} onClick={onClick} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
