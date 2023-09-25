4a0dff51dc4e9badcdbe2814552ad4f5cc238fc3
Pluto0104 <fed.dev.pluto@gmail.com>
2023-09-25 16:55:30 -0500

    define todo type

---
+++ src/types/todo.ts
@@ -0,0 +1,8 @@
+export type TodoType = {
+  id?: string;
+  title: string;
+  description: string;
+  isCompleted?: boolean;
+  createdAt?: string | number | Date;
+  updatedAt?: string | number | Date;
+};
