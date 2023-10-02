a48896f56da5fc4d6e6b4ecd0fcc4171842ccdd3
Pluto0104 <fed.dev.pluto@gmail.com>
2023-10-02 06:12:16 -0500

    support dynamic import class names

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  safelist: [
+    {
+      pattern: /border-(red|green|blue|yellow|gray)-(300|400|500)/, // You can display all the colors that you need
+      variants: ["focus"], // Optional
+    },
+    {
+      pattern: /bg-(red|green|blue|yellow|gray)-(300|400|500)/,
+      variants: ["hover"],
+    },
+    {
+      pattern: /text-(red|green|blue|yellow|gray)-(300|500)/,
+    },
+    {
+      pattern: /text-(xs|sm|md|lg|xl)/,
+    },
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
