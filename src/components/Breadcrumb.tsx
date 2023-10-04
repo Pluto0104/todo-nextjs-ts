11ffe89a90a8a94d44b8dae0664698f03dc39e88
Pluto0104 <fed.dev.pluto@gmail.com>
2023-10-04 17:07:33 -0500

    Update breadcrumb component

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -13,20 +13,31 @@ export interface BreadcrumbProps {
 
 const Breadcrumb: React.FC<BreadcrumbProps> = ({ items }) => {
   return (
-    <nav className="flex my-2" aria-label="Breadcrumb">
-      {items.map(({ icon, href, text }, index) => (
-        <div key={`breadcrumb-item-${index}`} className="flex items-center">
-          {icon && <span className="mr-1">{icon}</span>}
-          {href ? (
-            <Button href={href} className="hover:underline">
-              {text}
-            </Button>
-          ) : (
-            <span className="py-2 px-4">{text}</span>
-          )}
-          {index < items.length - 1 && <span className="mx-1">/</span>}
-        </div>
-      ))}
+    <nav className="w-full rounded-md my-2" aria-label="Breadcrumb">
+      <ol className="list-reset flex">
+        {items.map(({ icon, href, text }, index) => (
+          <li key={`breadcrumb-item-${index}`} className="flex items-center">
+            {icon && <span className="mr-1">{icon}</span>}
+            {href ? (
+              <Button
+                href={href}
+                className="text-white transition duration-150 ease-in-out hover:text-blue-600 focus:text-blue-600 active:text-blue-700"
+              >
+                {text}
+              </Button>
+            ) : (
+              <span className="px-2 text-neutral-500 dark:text-neutral-400">
+                {text}
+              </span>
+            )}
+            {index < items.length - 1 && (
+              <span className="mx-1 text-neutral-500 dark:text-neutral-400">
+                /
+              </span>
+            )}
+          </li>
+        ))}
+      </ol>
     </nav>
   );
 };
