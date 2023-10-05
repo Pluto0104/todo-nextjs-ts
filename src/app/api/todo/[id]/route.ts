ac8ea22829bea83487b5fbbb5b1428d1c41cbd18
Pluto0104 <fed.dev.pluto@gmail.com>
2023-10-05 18:50:02 -0500

    fixed backend bugs

--- src/app/api/todo/[id]/route.ts
+++ src/app/api/todo/[id]/route.ts
@@ -1,20 +1,50 @@
 import { NextResponse } from "next/server";
-import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
 import { SERVER_API_URL } from "@/config";
 import { TodoType } from "@/types/todo";
 
-export const GET = async (req: Request) => {
+export const GET = async (
+  req: Request,
+  { params: { id } }: { params: { id: string } }
+) => {
   try {
-    // const url = new URL(req.url);
-    // const { data } = await axios.get(`${SERVER_API_URL}/todos/${id}`);
-    // console.log(url, url.searchParams.keys(), url.searchParams.get("id"));
-    // const { id } = req.query;
-    return NextResponse.json({});
+    const { data: todo } = await axios.get(`${SERVER_API_URL}/todos/${id}`);
+    return NextResponse.json({ todo }, { status: 200 });
   } catch (err) {
+    throw err;
+  }
+};
+
+export const PATCH = async (
+  req: Request,
+  { params: { id } }: { params: { id: string } }
+) => {
+  try {
+    const todo: TodoType = await req.json();
+    await axios.patch(`${SERVER_API_URL}/todos/${id}`, {
+      ...todo,
+      updatedAt: Date.now(),
+    });
     return NextResponse.json(
-      {},
-      { status: 500, statusText: "Data fetching failed." }
+      { message: "The todo is updated." },
+      { status: 200 }
     );
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const DELETE = async (
+  req: Request,
+  { params: { id } }: { params: { id: string } }
+) => {
+  try {
+    await axios.delete(`${SERVER_API_URL}/todos/${id}`);
+    return NextResponse.json(
+      { message: "The todo is deleted successfully." },
+      { status: 200 }
+    );
+  } catch (err) {
+    throw err;
   }
 };
--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -7,13 +7,13 @@ import { TodoType } from "@/types/todo";
 export const GET = async (req: Request) => {
   try {
     const todos = await axios.get(`${SERVER_API_URL}/todos`);
-    return NextResponse.json({ code: 200, todos: todos.data });
+    return NextResponse.json({ todos: todos.data }, { status: 200 });
   } catch (err) {
-    return NextResponse.error();
+    throw err;
   }
 };
 
-export const POST = async (req: Request, response: Response) => {
+export const POST = async (req: Request) => {
   try {
     const todo: TodoType = await req.json();
     await axios.post(`${SERVER_API_URL}/todos`, {
@@ -21,34 +21,11 @@ export const POST = async (req: Request, response: Response) => {
       id: uuidv4(),
       createdAt: Date.now(),
     });
-    return NextResponse.json({ code: 200, message: "A new todo is created." });
+    return NextResponse.json(
+      { message: "A new todo is created." },
+      { status: 200 }
+    );
   } catch (err) {
-    console.error(err);
-    return NextResponse.json({ code: 500, message: "Internal Server Error" });
-  }
-};
-
-export const PUT = async (req: Request) => {
-  try {
-    const { id, ...rest }: TodoType = await req.json();
-    await axios.put(`${SERVER_API_URL}/todos/${id}`, {
-      ...rest,
-      updatedAt: Date.now(),
-    });
-    return NextResponse.json({ code: 200, message: "A new todo is created." });
-  } catch (err) {
-    console.error(err);
-    return NextResponse.json({ code: 500, message: "Internal Server Error" });
-  }
-};
-
-export const DELETE = async (req: Request) => {
-  try {
-    const { id } = await req.json();
-    await axios.delete(`${SERVER_API_URL}/todos/${id}`);
-    return NextResponse.json({ code: 200, message: "A new todo is created." });
-  } catch (err) {
-    console.error(err);
-    return NextResponse.json({ code: 500, message: "Internal Server Error" });
+    throw err;
   }
 };
